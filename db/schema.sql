5eb0c820efab9cc4d1fa4c3e9f3665aaa6d30488
jkouneski <jkouneski@yahoo.com>
2021-07-27 13:55:56 -0400

    role table

--- db/schema.sql
+++ db/schema.sql
@@ -5,17 +5,23 @@ CREATE DATABASE employees;
 USE employees;
 
 --department table--
-
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(25) NOT NULL,
     PRIMARY KEY (id)
 );
 
+--role table--
 CREATE TABLE role (
-    
-)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(25),
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
 
+--employee table--
 CREATE TABLE employee (
     
 )
