fa0f48da3ed618ffabdfabf70c7330758c1f240a
jkouneski <jkouneski@yahoo.com>
2021-07-27 13:49:10 -0400

    department table

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS employees;
+
+CREATE DATABASE employees;
+
+USE employees;
+
+--department table--
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(25) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    
+)
+
+CREATE TABLE employee (
+    
+)
